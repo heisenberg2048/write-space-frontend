e455ab1385aaaee4614b267e2161ea94b00a12a3
jay prakash <97428549+heisenberg2048@users.noreply.github.com>
2023-08-08 23:15:38 +0530

    Add files via upload

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import "./home.css";
+import axios from "axios";
+import { useLocation } from "react-router";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+  
+
+  useEffect(() => {
+    
+    const fetchPosts = async () => {
+      
+      const res = await axios.get(process.env.REACT_APP_API_URL+"/posts" + search);
+      console.log(search);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, [search]);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+      </div>
+    </>
+  );
+}
